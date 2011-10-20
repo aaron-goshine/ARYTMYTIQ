fbe6bdab6da5823ad0a706f54d668f44165c40ac
Goshine <greenlig@gmail.com>
2011-10-20 21:02:51 +0200

    --s

--- myApp.js
+++ myApp.js
@@ -5,18 +5,18 @@ window.onload = function(){
 	*
 	*useage 
 	*/
-	//diplayInMyConsole(ARQ().commaFmt(-100000000000.12343));
-	/*diplayInMyConsole(ARQ([10,1,3]).foreach(null,function(x){
+	//displayInMyConsole(ARQ().commaFmt(-100000000000.12343));
+	/*displayInMyConsole(ARQ([10,1,3]).foreach(null,function(x){
 		alert(x);
 		}));	*/
-	diplayInMyConsole(ARQ([100,13,10,34]).tiq('*'))
+	displayInMyConsole(ARQ([100,13,10,34]).tiq('*'))
 
 	//--	
 }
 
 
 
-function diplayInMyConsole(valuesToWrite){
+function displayInMyConsole(valuesToWrite){
 	
 	var cons = document.getElementById("console");
 	cons.innerHTML = valuesToWrite;
